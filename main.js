fc04d286051013e076a81da7bfeb27fede5dc0c1
Unknown <mizvladimir@gmail.com>
2019-03-19 10:24:26 +0100

    Buttons to start and stop FA

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
-sigmaInitCallback = function(s) {
-    sigma_instance.refresh();
-    sigma_instance.startForceAtlas2({
-        worker: true,
+forceAtlas2Config = {
+        // worker: true,
         barnesHutOptimize: true,
         // startingIterations: 50,
         scalingRatio: 200,
         iterationsPerRender: 10
-    });
+    }
+
+sigmaInitCallback = function(s) {
+    sigma_instance.refresh();
+    sigma_instance.startForceAtlas2(forceAtlas2Config);
     // window.setTimeout(sigma_instance.killForceAtlas2(), 20000);
+}
+
+function stopLayout(){
+    sigma_instance.killForceAtlas2();
+}
+
+function restartLayout(){
+    sigma_instance.startForceAtlas2(forceAtlas2Config);
 }
 
 var sigma_instance = new sigma({
